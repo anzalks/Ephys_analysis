083117bcf1892ad6ac54612f5a606a003f25b750
anzal <anzal.ks@gmail.com>
2019-08-22 00:10:00 +0530

    now the stimfitIO can be used to plot folder wise and save results in a results folder

--- load_dat_ephys_traces_raw_data_plotting_using_stimfit.py
+++ load_dat_ephys_traces_raw_data_plotting_using_stimfit.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 18 23:33:13 2019
-
-@author: anzal
-"""
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -13,52 +5,60 @@ Created on Sat Aug 17 17:48:05 2019
 
 @author: anzal
 """
+#"name:" inside the segments will give singnal unit/type of recording: use abf.read_block().segments[0].analogsignals[0].name to access it
+#channel index will tell which channel it is: abf.read_block().segments[0].analogsignals[0].annotations['channel_index'] (first segment, first signal, channel index)
+#the sampling rate is saved as an array in: abf.read_block().segments[0].analogsignals[0].sampling_rate (first segment, first signal, sampling rate, unit is also mentioned next to it)
+#str(abf.read_block().segments[0].analogsignals[0].sampling_rate).split()[1] will show the sampling rate units
+#str(abf.read_block().segments[0].analogsignals[0][0]).split()[1] will show the unit of singal recorded
 
+ 
 import os
-import numpy as np
 import neo.io as nio
+#import numpy as np
 import matplotlib.pyplot as plt
 
 #set the directory path with files to read
-folder_to_read = "/mnt/5D4B-FA71/Data/190814/"
-#list out all the files with .mat extension for plotting
+folder_to_read = "/mnt/5D4B-FA71/Data/190809/"
+#make a folder to save all the results
+os.mkdir(folder_to_read+'Results/')
+#make the file path
+results_folder = str(folder_to_read+'Results/')
+#list out all the files with .dat extension for plotting
 for root, dirs, files in os.walk(folder_to_read):
     for file in files:
         if file.endswith(".dat"):
-            print(file)
+            #print(file)
+            file_name = str(file).split(".")[0]
+            print (file_name)
             #import the file of interest
             file_to_read = nio.StimfitIO(root+file)
-            segments = ()
-            for trace in file_to_read.read_block().segments:
-                segments+= (trace,)
-                print (segments)
-                for segment in segments:
-                    for trace in segment.analogsignals:
-                        print(trace.sampling_rate)
-                        v = trace
-                        print(trace)
-                        ti = trace.t_start
-                        print(trace.t_start)
-                        tf = trace.t_stop
-                        print(trace.t_stop)
-                        tp = float(tf-ti)
-                        print(float(tf-ti))
-                        sr = float(trace.sampling_rate)
-                        print(float(trace.sampling_rate))
-                        t = np.linspace(0,tp,int(sr*tp))
-                        print(np.linspace(0,tp,int(sr*tp)))
-                        plt.plot(t,v)
-                        print(t,v)
-                        plt.show()
-                        
-                        
-                        
-test_file_to_read = "/mnt/5D4B-FA71/Data/190814/c1.dat"
-dat_file = nio.RawBinarySignalIO(test_file_to_read)
-dat_file
-
-
-
-dat_file_stimfit = nio.StimfitIO(test_file_to_read)
-dat_file_stimfit
-                        
+            segments = file_to_read.read_block().segments
+            #segments = ()
+            for segment in segments:
+                print(segment)
+                analog_signals = segment.analogsignals
+                print(analog_signals)
+                for trace in analog_signals:
+                    v = trace
+                    print(v)
+#                    if np.isnan(trace).all() == np.nan:
+#                       break
+#                       v = trace
+                    plt.plot(v)
+            #save the figure in the results folder with png as extension, fiile name, Key detail(trace number)     
+            plt.savefig(results_folder+file_name+" "+'_compiled.png')
+            plt.show()
+            for segment in segments:
+                print(segment)
+                analog_signals = segment.analogsignals
+                #print(analog_signals)
+                for trace in analog_signals:
+                    v = trace
+                    channel_index = str(trace.annotations['channel_index'])
+#                    if np.isnan(trace).all() != np.nan:
+#                        break
+#                        v = trace
+                    print (v)
+                    plt.plot(v)
+                    plt.savefig(results_folder+file_name+" "+str(trace.name)+" "+channel_index+""+str(trace.t_start)+'.png')
+                    plt.show()
