11b23aab1faecc7123a0947bab74fd87fec65cd8
anzalks <anzal.ks@gmail.com>
2021-02-10 19:04:49 +0530

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   .single_cell_unified_analysis.py.swp
    	modified:   single_cell_unified_analysis.py

--- scripts_with_parser/single_cell_unified_analysis.py
+++ scripts_with_parser/single_cell_unified_analysis.py
@@ -48,11 +48,11 @@ def data_file_filter(f_list):
     for f in f_list:
         p=str(f)
         reader = nio.AxonIO(p)
-        protocol_name = reader._axon_info['sProtocolPath']
-        protocol_name = str(protocol_name).split("\\")[-1]
-        protocol_name = protocol_name.split(".")[0]
-        protocol_num = protocol_name.split("_")[0]
-        if str.isdigit(protocol_num) == True:
+        prot_name = reader._axon_info['sProtocolPath']
+        prot_name = str(prot_name).split("\\")[-1]
+        prot_name = prot_name.split(".")[0]
+        prot_num = prot_name.split("_")[0]
+        if str.isdigit(prot_num) == True:
             r.append(f)
     f_list = r
     f_list.sort()
@@ -69,8 +69,6 @@ def protocol_name(f):
     protocol_name = str(protocol_name).split("\\")[-1]
     protocol_name = protocol_name.split(".")[0]
     protocol_num = protocol_name.split("_")[0]
-    print(f"number of protocols used in total = {protocol_num}")
-    print(f"names of protocols used in total = {protocol_name}")
     del(reader)
     return protocol_num, protocol_name
 
@@ -100,182 +98,67 @@ def cell_sorted_results(outdir,cell_set):
     for cell in cell_nos:
         Path.mkdir(Path.joinpath(outdir,f'cell_{cell}'),exist_ok=True, parents=True)
 
-def raw_trace_plot(f, folder_path):
+def plot_selector(protocol_index):
+    protocol_index = int(protocol_index)
+    similar_plots = [7,]
+    if protocol_index ==1:
+        plot_function = "series_res_check"
+    elif protocol_index ==2:
+        plot_function = "rmp"
+    elif protocol_index ==3:
+        plot_function  = "neuron_threshold"
+    elif protocol_index ==4:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "baseline_points"
+    elif protocol_index ==5:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "baseline_pattern"
+    elif protocol_index ==6:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "e_point"
+    elif protocol_index ==7:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "e_pattern"
+    elif protocol_index ==8:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "i_point"
+    elif protocol_index ==9:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "i_pattern"
+    elif protocol_index ==10:
+        #same function can be used for before and after training protocols use
+        #any in thatcase
+        plot_function = "training"
+    else:
+        plot_function = "something not a protocol"
+    return plot_function
+
+
+
+def raw_trace_plot(f):
 # use folder path in the previous loop to make use of the cell_*** folder path
 # allocation 
 #    columns = int(len(Vm_trail)/3)
     f = str(f)
     Vm_trail = []
-    sampling_rate
-    trace_unit
-    total_time
     reader = nio.AxonIO(filename=f)
     segments = reader.read_block().segments
     sample_trace = segments[0].analogsignals[0]
     sampling_rate = sample_trace.sampling_rate
     trace_unit = str(sample_trace.units).split()[1]
-    for si, segment in enumerate(segments):
-        analogsignals = segment.analogsignals
-        
-     rows = 3
- #    v = np.reshape(Vm_trail[1],(columns,rows))
- #    t = np.reshape(Vm_trail[0],(columns,rows))
- #    print(v.shape())
-     print("_____________________")
- #    print(f"reminder = {columns}")
-     fig, ax_array = plt.subplots(rows,2)
-     va_,vb_,vc_ = [],[],[]
-     ta_,tb_,tc_ = [],[],[]
-     na,nb,nc = 0,0,0
-     print(f"na {na} nb {nb} nc {nc}")
-
-     for ind_i,i in enumerate(Vm_trail):
-         r = ind_i%rows
-         a,b,c =(1%3,2%3,0)
-         v = Vm_trail[ind_i][0]
-         t = Vm_trail[ind_i][1]
-         if r == a:
-             na +=1
-             ta_.append(t)
-             va_.append(v)
-             ax_array[2][0].plot(t,v, alpha = 0.5)
-             if na == 3:
-                 va_ = np.mean(va_, axis = 0)
-                 ta_ = np.mean(ta_, axis = 0) 
- #                print(ta_,va_)
-                 ax_array[2][0].plot(ta_,va_, color = 'b', linewidth=0.5)
- 
- 
- #            ax_array[2].plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
- #            plt.subplot(rows,columns,ind_i)
- #            plt.plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
-         elif r == b:
-             nb +=1
-             vb_.append(v)
-             tb_.append(t)
-             ax_array[1][0].plot(t,v, alpha = 0.5)
-             if nb == 3:
-                 vb_ = np.mean(vb_, axis = 0)
-                 tb_ = np.mean(tb_, axis = 0) 
- #                print(tb_,vb_)
-                 ax_array[1][0].plot(tb_,vb_, color = 'k', linewidth=0.5)
- #                ax_array[1].plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
- #            plt.subplot(rows,columns,ind_i+1)
- #            plt.plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
-
-        elif r == b:
-             nb +=1
-             vb_.append(v)
-             tb_.append(t)
-             ax_array[1][0].plot(t,v, alpha = 0.5)
-             if nb == 3:
-                 vb_ = np.mean(vb_, axis = 0)
-                 tb_ = np.mean(tb_, axis = 0) 
- #                print(tb_,vb_)
-                 ax_array[1][0].plot(tb_,vb_, color = 'k', linewidth=0.5)
- #                ax_array[1].plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
- #            plt.subplot(rows,columns,ind_i+1)
- #            plt.plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
-         elif r == c:
-             nc +=1
-             vc_.append(v)
-             tc_.append(t)
-             ax_array[0][0].plot(t,v, alpha = 0.5, label=f'trial {nc}')
-             v_lowercut = np.copy(v)
- #            v_lowercut[v_lowercut<-50] = -50
-             time = np.copy(t)
-             v_smooth = v_lowercut
- #            v_smooth = np.ma.average(v_lowercut,axis=0)
- #            v_smooth = butter_bandpass_filter(v_lowercut,1,20, sampling_rate, order=1)
-             peaks = signal.find_peaks(x=v_smooth, height=-80,  threshold=None,
-                                       distance=50,
-                                       prominence=2.5, width=100, wlen=None,
-                                       rel_height=1,plateau_size=None)
- #            peaks_v = v_smooth
- #            peaks_t = time
-             peaks_t = t[peaks[0]-10]
-             peaks_v = v[peaks[0]-10]
-             ax_array[0][1].scatter(peaks_t,peaks_v,alpha=0.5, marker='.', 
-                                    label = f'peak response in trial {nc}')
-             print(f"peaks = {peaks_v}, {peaks_t}")
-             if nc == 3:
-                 vc_ = np.mean(vc_, axis = 0)
-                 tc_ = np.mean(tc_, axis = 0) 
- #                print(tc_,vc_)
-                 ax_array[0][0].plot(tc_,vc_, color = 'r', linewidth=0.5, label
-                                    = f'mean response for {nc} trails')
-                 ax_array[0][1].plot(tc_,vc_, color = 'r',alpha=0.3, linewidth=0.2)
- #            plt.subplot(rows,columns,ind_i+2)
- #            plt.plot(Vm_trail[ind_i][1],Vm_trail[ind_i][0])
- 
-     ax_array[2][0].set_title('TTL')
-     ax_array[2][0].set_xlabel('time(s)')
- #    ax_array[2].set_ylim(-0.5,2.5)
- #    ax_array[2].set_xlim(0.2,0.5)
-     ax_array[2][0].set_ylabel('V')
-     ax_array[1][0].set_title('photo diode')
- #    ax_array[1].set_ylim(2,7.5)
- #    ax_array[1].set_xlim(0.2,0.5)
-     ax_array[1][0].set_ylabel('pA')
-     ax_array[0][0].set_title('cell trace')
-     ax_array[0][0].set_ylim(-100,5)
-     ax_array[0][0].set_ylabel('mV')
- #    ax_array[0].set_xlim(0.2,0.5)
- #    plt.title("three channel recordings")
-     ax_array[0][1].set_title('Response peak spread')
-     ax_array[0][1].set_xlabel('time(s)')
-     ax_array[0][1].set_ylim(-100,5)
- 
-     ax_array[1,1].axis('off')
-     ax_array[2,1].axis('off')
-     fig.legend(title="Legend",borderaxespad=1, loc="lower right")
-     plt.tight_layout()
- 
-     outfile  savefig(*args, **kwargs) em)+ f" blipo_col_realing_1_resized.png"
-     plt.savefig(outfile,bbox_inches = 'tight',dpi = (300))
-     plt.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def total_trace(f):
-    global Vm_trail
-    global sampling_rate
-    global trace_unit
-    global toal_time
-    f= str(f)
-    Vm_trail = []
-    reader = nio.AxonIO(filename=f)
-    segments = reader.read_block().segments
-    sample_trace = segments[0].analogsignals[0]
-    sampling_rate = sample_trace.sampling_rate
-    trace_unit = str(sample_trace.units).split()[1]
-#    print(sampling_rate)
-#    print(trace_unit)
+    print(f"length of segments = {len(segments)}")
+    print(sampling_rate)
+    print(trace_unit)
     for si, segment in enumerate(segments):
         analog_signals = segment.analogsignals
         for trace in analog_signals:
+            print(f"length of each trace in the segments = {len(trace) }")
             v = trace
             v = np.ravel(v)
             v = v.magnitude
@@ -285,13 +168,11 @@ def total_trace(f):
             m = [v,t]
             Vm_trail.append(m)
             total_time = float(tf-ti)
-    del(reader)
-    return Vm_trail
 
-def series_res_check(f):
-    fig = plt.figure(figsize=(16,5))
-    ax1 = fig.add_subplot(121)
-    ax2 = fig.add_subplot(122)
+
+
+
+
 
 
 
@@ -306,12 +187,29 @@ def main(**kwargs):
     expt_num = tot_prot_num(f_list)
     cell_set = set_of_exp(f_list,expt_num)
     cell_sorted_results(outdir,cell_set)
+#    cell set will give the set of files which are of different protocols but
+#    from the same experiment.
     for cell in cell_set:
         print(f"protocol set length  --> {len(cell)}")
-        for pn, protocol in enumerate (cell):
+# f will have files in one set of protocols from a single experiment.        
+        for f in cell:
+            print(str(f))
+            f_prot = protocol_name(f)
+            print(f"protocol name = {f_prot[1]} protocol index number ="
+                  f"{f_prot[0]}")
+            protocol_index = f_prot[0]
+            print(f"protocol index = {protocol_index}")
+            prot = plot_selector(protocol_index)
+            print(f"selected protocol to plot = {prot}")
+            raw_trace_plot(f)
+#        for pn, protocol in enumerate (cell):
+#
 #plotting fucntion for each of the protocols            
-            print(f"path to the files = no.{pn} , {protocol}")
-
+#            print(f"path to the files = no.{pn} , {protocol}")
+#            for f in cell_set:
+#                f = str(f)
+#                raw_trace_plot(f)
+#
 #    print(f_list)
 #    expt_num = tot_prot_num(f_list)
 #    expt_num = expt_num
