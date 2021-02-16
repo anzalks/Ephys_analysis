feff274273a092350cf27be8ade50ca583de4b1d
anzalks <anzal.ks@gmail.com>
2021-02-16 14:07:58 +0530

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   .copy_single_cell.py.swp
    	modified:   .single_cell_unified_analysis.py.swp
    	modified:   single_cell_unified_analysis.py
    	deleted:    smple_abf/2020_12_04_0025.abf
    	deleted:    smple_abf/2020_12_04_0026.abf
    	deleted:    smple_abf/results/cell_0/2020_12_04_0025 input_R_check_.png
    	deleted:    smple_abf/results/cell_0/2020_12_04_0026 input_R_check_.png
    	deleted:    smple_abf/results/cell_0/2021_01_28_0000 input_R_check_.png
    	deleted:    smple_abf/results/cell_0/2021_01_28_0001 input_R_check_.png
    	deleted:    smple_abf/results/cell_1/2021_01_28_0002 input_R_check_.png
    	deleted:    smple_abf/results/cell_1/2021_01_28_0005 input_R_check_.png
    	deleted:    smple_abf/results/cell_1/2021_01_28_0006 input_R_check_.png
    	deleted:    smple_abf/results/cell_2/2021_01_28_0009 input_R_check_.png
    
     Untracked files:
    	smple_abf/2021_01_28_0010.abf
    	smple_abf/2021_01_28_0011.abf
    	smple_abf/2021_01_28_0012.abf
    	smple_abf/2021_01_28_0013.abf
    	smple_abf/2021_01_28_0014.abf
    	smple_abf/2021_01_28_0015.abf
    	smple_abf/2021_01_28_0016.abf
    	smple_abf/2021_01_28_0017.abf
    	smple_abf/2021_01_28_0018.abf
    	smple_abf/2021_01_28_0019.abf
    	smple_abf/2021_01_28_0020.abf
    	smple_abf/2021_01_28_0021.abf
    	smple_abf/2021_01_28_0022.abf
    	smple_abf/2021_01_28_0023.abf
    	smple_abf/2021_01_28_0024.abf
    	smple_abf/2021_01_28_0025.abf
    	smple_abf/2021_01_28_0026.abf
    	smple_abf/2021_01_28_0027.abf
    	smple_abf/2021_01_28_0028.abf
    	smple_abf/2021_01_28_0029.abf
    	smple_abf/2021_01_28_0030.abf
    	smple_abf/2021_01_28_0031.abf
    	smple_abf/2021_01_28_0032.abf
    	smple_abf/2021_01_28_0033.abf
    	smple_abf/2021_01_28_0034.abf
    	smple_abf/results/

--- scripts_with_parser/single_cell_unified_analysis.py
+++ scripts_with_parser/single_cell_unified_analysis.py
@@ -101,9 +101,9 @@ def cell_sorted_results(outdir,cell_set):
 def plot_selector(protocol_index):
     protocol_index = int(protocol_index)
     similar_plots = [7,]
-    if protocol_index ==1:
+    if protocol_index ==2:
         plot_function = "series_res_check"
-    elif protocol_index ==2:
+    elif protocol_index ==1:
         plot_function = "rmp"
     elif protocol_index ==3:
         plot_function  = "neuron_threshold"
@@ -139,7 +139,7 @@ def plot_selector(protocol_index):
         plot_function = "something not a protocol"
     return plot_function
 
-def series_res_check(Vm_trail,f,outdir):
+def series_res_check(Vm_trail, prot, f, outdir):
     f_str = str(f)
     reader = nio.AxonIO(f_str)
     protocols = reader.read_raw_protocol()
@@ -178,15 +178,16 @@ def series_res_check(Vm_trail,f,outdir):
     ax1.set_title('Recording')
     ax1.set_ylabel(trace_unit)
     ax1.set_xlabel('time(s)')
+    ax1.set_ylim(-90,-40)
     ax1.legend()
     ax2.set_title('Protocol trace')
     ax2.set_ylabel(protocol_unit)
     ax2.set_xlabel('time(s)')
     #        ax2.legend()
-    plt.figtext(0.10,-0.05,f"Input resistance (averaged from {iter_num} traces) =" 
-                f"+str(np.around(mean_R,decimals =2))"
+    plt.figtext(0.10,-0.05,f"Input resistance averaged from {iter_num} traces =" 
+                f"{str(np.around(mean_R,decimals =2))}"
                 f" MOhm ", fontsize=12, va="top", ha="left")
-    plt.suptitle(f'Protocol type: current calmp - Input_res_check',fontsize=15)
+    plt.suptitle(f'Protocol type: {prot}',fontsize=15)
     plt.figtext(0.10, -0.10, f"sampling rate = {sampling_rate}" ,
                 fontsize=12, va="top", ha="left" )
 #    plt.figtext(0.10, -0.15, f"total recording time ="
@@ -197,6 +198,63 @@ def series_res_check(Vm_trail,f,outdir):
     print("-----> Saved to %s" % outfile)
     fig = plt.close()
     del(reader)
+
+def Base_line_protocol(Vm_trail, prot, f, outdir):
+    f_str = str(f)
+    reader = nio.AxonIO(f_str)
+    protocols = reader.read_raw_protocol()
+    protocol_unit = clamp_stat = protocols[2][0]
+    segments = reader.read_block().segments
+    sample_trace = segments[0].analogsignals[0]
+    sampling_rate = sample_trace.sampling_rate
+    trace_unit = str(sample_trace.units).split()[1]
+    fig = plt.figure(figsize=(16,5))
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    RMP = []
+    iter_num = 0
+    for v in  enumerate(Vm_trail):
+        trace_number = v[0]
+        trace = v[1][0]
+        time = v[1][1]
+        mean_RMP = np.mean(trace)
+        cv_rmp = float((np.std(trace))/mean_RMP)
+        if mean_RMP <-50:
+            iter_num +=1
+            ax1.plot(time, trace, label=f'trace no. {iter_num}', alpha = 0.5)
+            P_traces= protocols[0]
+            RMP = mean_RMP
+            for p in P_traces:
+                for i in p:
+                    t_ = len(i)/sampling_rate
+                    t = np.linspace(0,float(t_), len(i))
+                    ax2.plot(t,i)
+#        First_inj = mpatches.Patch(color='green', label='First injection')
+#        Thres_inj = mpatches.Patch(color='black', label='Threshold injection')
+#        Last_inj = mpatches.Patch(color='red', label='Final injection')
+#        ax2.legend(handles=[First_inj,Thres_inj,Last_inj])
+    ax1.set_title('Recording')
+    ax1.set_ylabel(trace_unit)
+    ax1.set_xlabel('time(s)')
+    ax1.legend()
+    ax1.set_ylim(-90,-20)
+    ax2.set_title('Protocol trace')
+    ax2.set_ylabel(protocol_unit)
+    ax2.set_xlabel('time(s)')
+    #        ax2.legend()
+    plt.figtext(0.10,-0.05, "Resting membrane potential average from"
+                f" {iter_num} traces= "+
+                str(np.around(RMP,decimals = 2))+" mV", fontsize=12, va="top", ha="left"    )
+    plt.suptitle(f'Protocol type: {prot}',fontsize=15)
+    plt.figtext(0.10, -0.10, f"sampling rate = {sampling_rate}" ,
+                fontsize=12, va="top", ha="left" )
+    plt.figtext(0.10, -0.15, f" = cv of trace = {np.arounc(cv_rmp, decimals= 3)}" ,
+                fontsize=12, va="top", ha="left")
+    outfile = str(outdir)+"/"+str(f.stem)+ f" {prot}.png"
+    plt.savefig(outfile,bbox_inches = 'tight')
+    print("-----> Saved to %s" % outfile)
+    fig = plt.close()
+
 
 
 def raw_trace(f):
@@ -260,7 +318,15 @@ def main(**kwargs):
             print(f"selected protocol to plot = {prot}")
             Vm_trail = raw_trace(f)
             if prot =="series_res_check":
-                series_res_check(Vm_trail,f,outdir)
+                try:
+                    series_res_check(Vm_trail, prot, f, outdir)
+                except:
+                    pass
+            elif prot == "rmp":
+                try:
+                    Base_line_protocol(Vm_trail, prot, f, outdir)
+                except:
+                    pass
             else:
                 print("not a series_res_check protocol")
 
