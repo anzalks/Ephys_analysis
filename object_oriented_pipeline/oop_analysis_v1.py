a56efd005f80660f89a7cd9ec7eb0728d8f8bc0c
anzalks <anzal.ks@gmail.com>
2021-05-13 22:20:29 +0530

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	deleted:    .class_dev.py.swp
    	modified:   oop_analysis_v1.py
    
     Untracked files:
    	../.gitignore
    	../Finished/
    	../Finished_without_parser/
    	../Protocol_reading_abf.py
    	../Test_allen_sdk_on_ploting_for_Voltage_clamp-temp.py
    	../Test_allen_sdk_on_ploting_for_Voltage_clamp_abf.py
    	../Test_allen_sdk_on_ploting_for_current_clamp_abf.py
    	../a.png
    	../analysis_current_clamp.py
    	../analysis_current_clamp_plot_for_all_traces_in_one_graph.py
    	../argparse_test.py
    	../generated_protocol.atf
    	../not_yet_ready/
    	.multi_protocol_analysis.py.swp
    	../plot_sub_plots/
    	../randomness/
    	../req.txt
    	../sum.py
    	../test_parser.py

--- object_oriented_pipeline/oop_analysis_v1.py
+++ object_oriented_pipeline/oop_analysis_v1.py
@@ -56,7 +56,7 @@ def raw_trace(f):
             tf = trace.t_stop
             ti = trace.t_start
             t = np.linspace(0,float(tf - ti), len(v))
-            m = [v,t]
+            m = [t,v]
             Vm_trail.append(m)
             total_time = float(tf-ti)
     return Vm_trail
@@ -65,10 +65,9 @@ def protol_trace(raw_protocol):
     pro_traces = raw_protocol[0]
     print(f"the length of raw prot trace = {len(pro_traces)}")
     for trace_no,trace in enumerate(pro_traces):
-        print(f"###########......the {trace_no}th with length"
-              f"{len(trace)}trace is {trace}.......############")
+        print("ptrace")
 
-class file_details:
+class File_details:
     def __init__(self,p):
         self.p= p
         self.day_folder = list_files(p)
@@ -88,18 +87,23 @@ class file_details:
 
 
 
-class protocols:
-    def __init__(self,cell_no, cell):
-        self.cell_data_files = cell
-        cell_ = self.cell_data_files
-        self.raw_trace = []
-        self.raw_protocol = []
-        for file_ in cell_:
-            abf_file(file_)
 
 
-def analysis1( protocol ):
-    # do something
+
+
+def find_baseline( recording ):
+    r = recording[0]
+    t = r[0]
+    v = r[1]
+    y_max = max(v)
+    y_min = min(v)
+    x_min = min(t)
+    x_max = max(t)
+    rmp = np.around(np.average(v), decimals = 2)
+    plt.plot(t,v)
+    plt.ylim(y_min-10, y_max+10 )
+    plt.title(str(rmp))
+    plt.show()
     print( "analysis1" )
 
 def analysis2( protocol ):
@@ -155,10 +159,53 @@ def analysis14( protocol ):
     print( "analysis3" )       
 
 
+def Base_line_plot(f, fi, rec_trace, sampling_rate,trace_unit, protocol_unit):
+    fig = plt.figure(figsize=(16,5))
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    RMP = []
+    iter_num = 0
+    for v in  enumerate(rec_trace):
+        trace_number = v[0]
+        trace = v[1][0]
+        time = v[1][1]
+        mean_RMP = np.mean(trace)
+        if mean_RMP <-50:
+            iter_num +=1
+            ax1.plot(time, trace, label=f'trace no. {iter_num}', alpha = 0.5)
+            P_traces= protocols[0]
+            RMP = mean_RMP
+            for p in P_traces:
+                for i in p:
+                    t_ = len(i)/sampling_rate
+                    t = np.linspace(0,float(t_), len(i))
+                    ax2.plot(t,i)
+#        First_inj = mpatches.Patch(color='green', label='First injection')
+#        Thres_inj = mpatches.Patch(color='black', label='Threshold injection')
+#        Last_inj = mpatches.Patch(color='red', label='Final injection')
+#        ax2.legend(handles=[First_inj,Thres_inj,Last_inj])
+    ax1.set_title('Recording')
+    ax1.set_ylabel(trace_unit)
+    ax1.set_xlabel('time(s)')
+    ax1.legend()
+    ax1.set_ylim(-90,-20)
+    ax2.set_title('Protocol trace')
+    ax2.set_ylabel(protocol_unit)
+    ax2.set_xlabel('time(s)')
+    #        ax2.legend()
+    plt.figtext(0.10,-0.05, "Resting membrane potential average from"
+                f" {iter_num} traces= "+
+                str(np.around(RMP,decimals = 2))+" mV", fontsize=12, va="top", ha="left")
+    plt.suptitle(f'Protocol type: {protocol_type} - {protocol_used}',fontsize=15)
+    plt.figtext(0.10, -0.10, f"sampling rate = {sampling_rate}" ,
+                fontsize=12, va="top", ha="left" )
+    plt.figtext(0.10, -0.15, f"total recording time = {total_time} s" ,
+                fontsize=12, va="top", ha="left")
+    outfile = str(outdir)+"/"+str(f.stem)+ f" {protocol_used}_{fi}.png"
+    plt.savefig(outfile,bbox_inches = 'tight')
+    print("-----> Saved to %s" % outfile)
+    fig = plt.close()
 
-def plot1( protocol ):
-    # do something
-    print( "analysis1" )
 
 def plot2( protocol ):
     # do something
@@ -213,7 +260,7 @@ def plot14( protocol ):
     print( "analysis3" ) 
 
 
-proto_map = { "1_RMP_gap_free_1min": ( analysis1, plot1 ), 
+proto_map = { "1_RMP_gap_free_1min": ( find_baseline, Base_line_plot ), 
              "2_Input_resistance_50pA_300ms": (analysis2, plot2), 
              "3_cell_threshold_10pA_step_-50_140pA_500ms": (analysis3, plot3 ),
              "4_Baseline_point_by_point_response_25_x_25_grid"
@@ -239,7 +286,30 @@ proto_map = { "1_RMP_gap_free_1min": ( analysis1, plot1 ),
 
             }
 
-proto_sequence = sorted(proto_map)
+proto_sequence = ["1_RMP_gap_free_1min", 
+                  "2_Input_resistance_50pA_300ms", 
+                  "3_cell_threshold_10pA_step_-50_140pA_500ms",
+                  "4_Baseline_point_by_point_response_25_x_25_grid"
+                  "_size_50ms_pulse_300ms_spaced",
+                  "5_Baseline_Excitation_point_by_point_response_25"
+                  "_x_25_grid_size_50ms_pulse_200ms_spaced",
+                  "6_Baseline_Inhibition_point_by_point_response"
+                  "_25_x_25_grid_size_50ms_pulse_200ms_spaced",
+                  "7_10_patterns_Base_line_2s_50ms_frames_3"
+                  "_times_repeat",
+                  "8_10_Pattern_baseline_excitation_0mV_holding"
+                  "_25X25_single_point",
+                  "9_10_Pattern_baseline_Inhibition-70mV_holding"
+                  "_25X25_single_point",
+                  "10_Pattern_training_every_2s_50ms_frames_3_times"
+                  "_repeat_one_depolarised_frame",
+                  "11_Inhibition_point_by_point_response_25_x_25"
+                  "_grid_size_50ms_pulse_200ms_spaced",
+                  "12_Inhibition_point_by_point_response_25_x_25"
+                  "_grid_size_50ms_pulse_200ms_spaced",
+                  "13_10_Pattern_Inhibition-70mV_holding_25X25_single_point",
+                  "14_10_Pattern_Inhibition-70mV_holding_25X25_single_point"
+                 ] 
 
 
 class Protocol:
@@ -252,7 +322,6 @@ class Protocol:
         prot_name = prot_name.split(".")[-2]
 
         self.protocol_name = prot_name
-        print(self.protocol_name)
         self.analysis_func = proto_map[prot_name][0]
         self.plot_func = proto_map[prot_name][1]
         self.recording = raw_trace(abf)
@@ -261,9 +330,6 @@ class Protocol:
         self.sampling_rate = reader._sampling_rate
         self.protol_unit = self.protocol_raw_data[2][0]
         #self.supported_obj = reader.supported_objects
-        print(f"++++++{self.name}+++++++++")
-        print(f".......{self.protocol_name}........")
-        print(f"'''''''{self.sampling_rate}'''''''''''''")
 
 
 def main():
@@ -281,22 +347,18 @@ def main():
     for f in fpaths:
         f =str(f)
         if f.split(".")[-1] == "abf":
-            print( f )
             try:
                 proto = Protocol( f )
                 protocols[proto.protocol_name] = proto
             except:
                 continue
 
-
     # Do analysis
-    for p in proto_sequence:
-        protocols[p].analysis_func()
-
+    for p in proto_sequence[0:2]:
+        protocols[p].analysis_func(protocols[p].recording)
     # Do plotting
-    for p in proto_sequence:
-        protocols[p].plot_func()
-
+#    for p in proto_sequence:
+#        protocols[p].plot_func()
 
 if __name__  == '__main__':
     main()
